8fb785ffc65c84f4dfe765f3374a59d337523262
Melanie van den Bosch <melanie@SSB12002.wurnet.nl>
2016-11-10 14:36:20 +0100

    script to retrieve genomes from MicroDB

---
+++ get_genomes_microDB.R
@@ -0,0 +1,38 @@
+##Author: Melanie van den Bosch
+##Script for retrieving all the genomes from the 
+##MicroDB namescpace on blazegraph
+
+
+# install the needed packages
+install.packages("RCurl")
+install.packages("XML")
+# SPARQL needs RCurl and XML
+install.packages("SPARQL")
+
+# loading required library to execute SPARQL query
+library("SPARQL")
+
+#---------Retrieving genomes from MicroDB--------#
+genomes <- "
+PREFIX ssb:<http://csb.wur.nl/genome/>
+SELECT DISTINCT ?genome ?organism
+WHERE {
+?genome a ssb:Genome .
+?genome ssb:organism ?organism .
+} 
+ORDER BY ?organism
+"
+
+endpoint <- "http://ssb2:9999/blazegraph/namespace/MicroDB/sparql/MicroDB/sparql"
+
+# We want to retrieve the genomes with the location of it on the web
+output <- SPARQL(url = endpoint, query = genomes)
+
+# 
+genomes.with.names <- output$results
+
+write.table(genomes.with.names, file = "genomes.csv", 
+            append = F, sep = ",", row.names = FALSE, 
+            quote = FALSE, col.names = FALSE)
+
+
