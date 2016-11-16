6a737b479f0ae8032ab9df15549b722a0186fd45
Melanie van den Bosch <melanie@SSB12002.wurnet.nl>
2016-11-16 16:12:05 +0100

    script to compute weight table of reference seqs

---
+++ compute_reference_weight_tables.R
@@ -0,0 +1,75 @@
+#############################################################################
+####  Author: Melanie van den Bosch
+####  Script for retrieving ribosomal domains of a genome
+####  and then computing a reference weight table for every genome
+#################################################################################
+
+# install the needed packages
+install.packages("RCurl")
+install.packages("XML")
+# SPARQL needs RCurl and XML
+install.packages("SPARQL")
+# Packages needed to be installed to calculate the frequency of oligonucleotides
+source("http://bioconductor.org/biocLite.R")
+?BiocUpgrade
+biocLite("BiocUpgrade")
+
+
+# loading required library to execute SPARQL query
+library("SPARQL")
+# loading required library to compute the codon frequency
+library("Biostrings")
+
+##-------- Computing reference weight table  ---------------###
+
+# based on ribosomal protein domains
+# We will go through all the genomes and the weight tables 
+# will be written to a file
+# creating a variable to store all the ribosomal proteins from all Acinetobacter baumannii genomes
+# The output contains CDSsequence and proteinsequence, however 157 distinct CDS are obtained
+
+ribosomal.seqs <- "
+PREFIX ssb:<http://csb.wur.nl/genome/>
+PREFIX biopax:<http://www.biopax.org/release/bp-level3.owl#>
+SELECT DISTINCT ?Pfam_id ?domain_description ?d_begin ?d_end ?cds_seq
+WHERE {
+  VALUES ?genome { <http://csb.wur.nl/genome/GCA_000746605-1> }
+  ?genome a ssb:Genome .
+  ?genome ssb:organism ?organism .
+  ?genome ssb:dnaobject ?dna .
+  ?dna ssb:feature ?cds .
+  ?cds a ssb:Cds .
+  ?cds ssb:sequence ?cds_seq .
+  ?cds ssb:protein ?protein .
+  #?protein ssb:sequence ?p_seq .
+  ?cds ssb:tool 'prodigal' .
+  ?protein ssb:feature ?domain .
+  ?domain ssb:begin ?d_begin .
+  ?domain ssb:end ?d_end .
+  ?domain ssb:signature ?signature .
+  ?signature biopax:xref ?xref .
+  ?xref biopax:db 'pfam' .
+  ?xref biopax:id ?Pfam_id .
+  ?signature ssb:interpro_description ?domain_description
+  FILTER(regex(?domain_description, 'ribosomal protein', 'i'))
+}
+"
+
+endpoint <- "http://ssb4:9999/blazegraph/namespace/MicroDB/sparql/MicroDB/sparql"
+#storing the output of the query.
+
+output1 <- SPARQL(url = endpoint, query = AB1ribo, ns = c("ssb","http://csb.wur.nl/genome/"))
+data1 <- data.frame(output1$results, stringsAsFactors=F)
+
+
+for (genomeID in genomes) { #always put he { on this line
+  fileout <- paste(outfolder, genomeID, ".csv", sep="")
+  #check if file already exists
+  if (!file.exists(fileout)) {
+    genome.sub <- sub("xxx", genomeID, domain.sparql)
+    output.all <- SPARQL(url = endpoint, query = genome.sub)
+    domain.data <- output.all$results
+    write.table(domain.data, file=fileout, append=F, sep = ",",
+                row.names = F, quote=F, col.names=T )
+  }
+}
