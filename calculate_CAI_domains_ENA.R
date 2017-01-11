2ef6f2fcdb83032a01a085df9fc6fd31a31b95ff
meeldurb <meeldurb@gmail.com>
2017-01-11 17:00:38 +0100

    added a cat to visualize at which genome the for loop is.

--- calculate_CAI_domains_ENA.R
+++ calculate_CAI_domains_ENA.R
@@ -37,6 +37,7 @@ genome.and.organisms <- read.csv(file = "genomes_ENA.csv", header = FALSE,
 # calculating the codon adaptation index for all genomes and its domains
 
 for (genomeID in genome.and.organisms[,1]) { 
+  cat (genomeID, "\n")
   fileout <- paste(outfolder, genomeID, "_CAI.csv", sep="")
   #check if file already exists
   if (!file.exists(fileout)) {
@@ -115,71 +116,71 @@ for (genomeID in genome.and.organisms[,1]) {
 
 ####################_____________ calculating cai for single file _______________________################
 
-# Retrieving weight vectors
-w.data <- read.csv(file = "Reference_weight_tables_ENA/GCA_000003645.csv", 
-                   header = FALSE, row.names = 1, as.is=TRUE) #as.is to keep the it as char
-
-# ordering on rownumbers for it to look the same as caitab
-w.data$names <-rownames(w.data)
-ordered.w <- w.data[with(w.data, order(names)), ]
-
-# only having numbers
-w <- w.data[,1]
-w.ordered <- ordered.w[,1]
-
-# retrieving domain data
-domain.data <- read.csv(file = "Domain_data_ENA/GCA_000003645.csv", header = TRUE, 
-                        as.is=TRUE) #as.is to keep the it as char
-
-# renaming the columns
-colnames(domain.data) <- c("domain_ID", "domain_beginpos", "domain_endpos", "CDS")
-# checking how df looks like
-str(domain.data)
-
-####-----computing the postions of the domains with their corresponding postion in the CDS-----###
-
-for (row in domain.data) 
-{
-  CDS_begin <- 3*(domain.data[2]-1)+1
-  CDS_end <- 3*(domain.data[3])
-}
-
-# renaming the columns
-colnames(CDS_begin) <- ""
-colnames(CDS_end) <- ""
-
-# binding these columns to already existing dataframe
-# and converting to integers for later calculation
-domain.data$CDS_domain_beginpos <- as.integer(CDS_begin[,1])
-domain.data$CDS_domain_end <- as.integer(CDS_end[,1])
-str(domain.data)
-
-# iterate over all the CDS and "substring" the CDS that is associated to the protein domains
-for (CDS in domain.data[4]){
-  dom_seq <- substr(CDS, domain.data[domain.data$CDS == CDS, 5], 
-                    domain.data[domain.data$CDS == CDS, 6])
-  
-}
-domain.data$CDS_domain <- dom_seq 
-
-# shrinking the data to reduce computational time
-keep <- c("domain_ID", "CDS_domain")
-domain.data.CDS <- domain.data[keep]
-
-write.table(domain.data.CDS, file = "tmp.csv", append = FALSE, sep = ",", row.names = FALSE, quote = FALSE, col.names = FALSE)
-
-# converting the sequences to fasta format with python file
-convertcmd <- "python Write_csvtofasta.py"
-system(convertcmd)
-
-# opening the written file and calculating the CAI
-fasta.domains <- read.fasta(file = "tmp.fasta")
-cai.output <- sapply(fasta.domains, cai, w = w)
-cai.output.ord <- sapply(fasta.domains, cai, w = w.ordered, numcode = 1)
-
-# write the data to a file
-write.table(cai.output, file = "CAI_GCA_000003645.csv", append = F, sep = "\t", row.names = names(cai.output), quote = F, col.names = F)
-write.table(cai.output.ord, file = "CAI_GCA_000003645ord2.csv", append = F, sep = "\t", row.names = names(cai.output.ord), quote = F, col.names = F)
-
-# gives different output.
-# found that cai function finds the indexpositions of codons that should be excluded from the analysis based on alphabetical order
+# # Retrieving weight vectors
+# w.data <- read.csv(file = "Reference_weight_tables_ENA/GCA_000003645.csv", 
+#                    header = FALSE, row.names = 1, as.is=TRUE) #as.is to keep the it as char
+# 
+# # ordering on rownumbers for it to look the same as caitab
+# w.data$names <-rownames(w.data)
+# ordered.w <- w.data[with(w.data, order(names)), ]
+# 
+# # only having numbers
+# w <- w.data[,1]
+# w.ordered <- ordered.w[,1]
+# 
+# # retrieving domain data
+# domain.data <- read.csv(file = "Domain_data_ENA/GCA_000003645.csv", header = TRUE, 
+#                         as.is=TRUE) #as.is to keep the it as char
+# 
+# # renaming the columns
+# colnames(domain.data) <- c("domain_ID", "domain_beginpos", "domain_endpos", "CDS")
+# # checking how df looks like
+# str(domain.data)
+# 
+# ####-----computing the postions of the domains with their corresponding postion in the CDS-----###
+# 
+# for (row in domain.data) 
+# {
+#   CDS_begin <- 3*(domain.data[2]-1)+1
+#   CDS_end <- 3*(domain.data[3])
+# }
+# 
+# # renaming the columns
+# colnames(CDS_begin) <- ""
+# colnames(CDS_end) <- ""
+# 
+# # binding these columns to already existing dataframe
+# # and converting to integers for later calculation
+# domain.data$CDS_domain_beginpos <- as.integer(CDS_begin[,1])
+# domain.data$CDS_domain_end <- as.integer(CDS_end[,1])
+# str(domain.data)
+# 
+# # iterate over all the CDS and "substring" the CDS that is associated to the protein domains
+# for (CDS in domain.data[4]){
+#   dom_seq <- substr(CDS, domain.data[domain.data$CDS == CDS, 5], 
+#                     domain.data[domain.data$CDS == CDS, 6])
+#   
+# }
+# domain.data$CDS_domain <- dom_seq 
+# 
+# # shrinking the data to reduce computational time
+# keep <- c("domain_ID", "CDS_domain")
+# domain.data.CDS <- domain.data[keep]
+# 
+# write.table(domain.data.CDS, file = "tmp.csv", append = FALSE, sep = ",", row.names = FALSE, quote = FALSE, col.names = FALSE)
+# 
+# # converting the sequences to fasta format with python file
+# convertcmd <- "python Write_csvtofasta.py"
+# system(convertcmd)
+# 
+# # opening the written file and calculating the CAI
+# fasta.domains <- read.fasta(file = "tmp.fasta")
+# cai.output <- sapply(fasta.domains, cai, w = w)
+# cai.output.ord <- sapply(fasta.domains, cai, w = w.ordered, numcode = 1)
+# 
+# # write the data to a file
+# write.table(cai.output, file = "CAI_GCA_000003645.csv", append = F, sep = "\t", row.names = names(cai.output), quote = F, col.names = F)
+# write.table(cai.output.ord, file = "CAI_GCA_000003645ord2.csv", append = F, sep = "\t", row.names = names(cai.output.ord), quote = F, col.names = F)
+# 
+# # gives different output.
+# # found that cai function finds the indexpositions of codons that should be excluded from the analysis based on alphabetical order
