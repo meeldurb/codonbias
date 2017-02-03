76cc44be842ed130300f524c5e3eefb590eb0914
meeldurb <meeldurb@gmail.com>
2017-02-03 18:00:31 +0100

    changed the filenames

--- Write_genecsvtofasta.py
+++ Write_genecsvtofasta.py
@@ -14,10 +14,13 @@ def write_to_fasta(file_input, file_output):
             CDS_gene = column[1]
             file_output.write('>%s\n%s\n' %(ID, CDS_gene))
             
-            
+#def get_file(argv):
+ #   """ Retrieving and opening file from cmd line 
+  #  """
+   # file_name = argv[1]            
         
 
 
-with open('tmp.csv','r') as file_input:
-    with open('tmp.fasta', 'w') as file_output:
+with open('tmpcai.csv','r') as file_input:
+    with open('tmpcai.fasta', 'w') as file_output:
         write_to_fasta(file_input, file_output)
