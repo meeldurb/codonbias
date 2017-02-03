512fef340ff3953c5920582542ec7665a23cdb66
meeldurb <meeldurb@gmail.com>
2017-02-03 16:27:10 +0100

    ?

--- iterated_refweighttables_ENA.R
+++ iterated_refweighttables_ENA.R
@@ -9,7 +9,7 @@
 ####  domains are comparable to previous iteration
 #################################################################################
 
-# Packages needed to be installed to calculate the frequency of oligonucleotides
+# install needed packages
 source("http://bioconductor.org/biocLite.R")
 ?BiocUpgrade
 biocLite("Biostrings")
@@ -17,6 +17,12 @@ biocLite("Biostrings")
 # loading required libraries
 library("Biostrings")
 library("seqinr")
+
+#loading my own functions
+source("/home/melanie/Documents/Master_Thesis_SSB/git_scripts/cweight.R")
+
+
+
 
 setwd("~/Documents/Master_Thesis_SSB/git_scripts")
 
@@ -30,10 +36,10 @@ gene.data <- read.csv(file = gene.files, header = TRUE,
                         as.is=TRUE) #as.is to keep the it as char
 colnames(gene.data) <- c("gene_ID", "CDS")
 
-
 #weight table
 w.files <- paste("Reference_weight_tables_ENA/", genomeID, ".csv", sep = "")
 w.data <- read.csv(file = w.files, header = FALSE, as.is = TRUE)
+# order on codon because of cai function
 ordered.w <- w.data[with(w.data, order(V1)), ]
 # only leaving numbers
 w <- ordered.w[,2]
@@ -41,17 +47,29 @@ w <- ordered.w[,2]
 #cai values of the first round
 cai.files <- paste("CAI_CDS/", genomeID, "_CAI_CDS.csv", sep = "")
 cai.data <- read.csv(file = cai.files, header = TRUE, as.is = TRUE)
+
+
+
+
+
 # sort on CAI value and take top 50
 sort.cai <- cai.data[order(-cai.data$cai.output),]
 ini.top50 <- head(sort.cai, 50)
 
 # We retrieve only the CDS of the gene_IDs that are in the top 50
-match.id <- as.vector(top50[,1])
+match.id <- as.vector(ini.top50[,1])
 gene.match <- gene.data[gene.data$gene_ID %in% match.id,] 
 
+codon.table <- compute.weight(gene.match[,2], "GCA_000003645")
 
 
-####
+# We re-calculate the CAI of all the gene_IDs 
+
+
+
+write.table(gene.data, file = "tmpcai.csv", append = FALSE, sep = ",", 
+            row.names = FALSE, quote = FALSE, col.names = FALSE)
+
 
 
 
