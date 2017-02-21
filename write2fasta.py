91abcd07b9bf77a192693ce2cf6de9e7bd25b4e5
meeldurb <meeldurb@gmail.com>
2017-02-21 13:55:14 +0100

    added main condition, now script runs

--- write2fasta.py
+++ write2fasta.py
@@ -4,20 +4,21 @@ Script: Converting a csv input file of
 sequences from SPARQL query to a fasta file .
 """
 
+from sys import argv
 
 def get_filenames(argv):
     """ Retrieving and opening file from cmd line 
     """
     file_input = argv[1]
     file_output = argv[2]
+    return file_input, file_output
 
 def write_fasta(file_input, file_output):
-    """ Write files to fasta
+    """ Write csv files to fasta
     """
-
     with open(file_input,'r') as csv_input:
         with open(file_output, 'w') as fasta_output:
-            convert_to_fasta(file_input, file_output)
+            convert_to_fasta(csv_input, fasta_output)
 
 
 
@@ -36,7 +37,11 @@ def convert_to_fasta(file_input, file_output):
             CDS_dom = column[1]
             file_output.write('>%s\n%s\n' %(ID, CDS_dom))
                     
-            
+if __name__ == "__main__": 
+    # get filenames   
+    file_input, file_output = get_filenames(argv)
+    # open and write to file
+    write_fasta(file_input, file_output)       
             
 
                     
