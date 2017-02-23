be23b18e4f13ceb3edaf4f94301c08bf574423ca
meeldurb <meeldurb@gmail.com>
2017-02-23 15:09:35 +0100

    changed to cCAI function and new weight tables

--- calculate_CAI_CDS.R
+++ calculate_CAI_CDS.R
@@ -23,11 +23,12 @@ library("SPARQL")
 library("Biostrings")
 # loading required library to use CAI function
 library("seqinr")
-
+#loading my own functions
+source("/home/melanie/Documents/Master_Thesis_SSB/git_scripts/cCAI.R")
 
 setwd("~/Documents/Master_Thesis_SSB/git_scripts")
 
-outfolder <- "CAI_CDS/"  
+outfolder <- "new_CAI_CDS/"  
 if (!file.exists(outfolder))dir.create(outfolder)
 
 
@@ -35,16 +36,18 @@ if (!file.exists(outfolder))dir.create(outfolder)
 genome.and.organisms <- read.csv(file = "genomes_ENA.csv", header = FALSE, 
                                  as.is=TRUE) #as.is to keep the it as char
 
+genomeID <- "GCA_000003645"
+
 
 # calculating the codon adaptation index for all genomes and its complete CDS
 
 for (genomeID in genome.and.organisms[,1]) { 
   cat (genomeID, "\n")
-  fileout <- paste(outfolder, genomeID, "_CAI_CDS.csv", sep="")
+  fileout <- paste(outfolder, genomeID, "_CAI_CDS_new.csv", sep="")
   #check if file already exists
   if (!file.exists(fileout)) {
     # Going through all genome numbers and retrieving their weight vectors and domain data
-    w.files <- paste("Reference_weight_tables_ENA/", genomeID, ".csv", sep = "")
+    w.files <- paste("Iterated_weight_tables_ENA/", genomeID, "_it_weight.csv", sep = "")
     if (file.exists(w.files)){
       w.data <- read.csv(file = w.files, header = FALSE, as.is = TRUE)
       # ordering on rownames, for it to be correctly used by cai function
@@ -55,77 +58,78 @@ for (genomeID in genome.and.organisms[,1]) {
       gene.data <- read.csv(file = gene.files, header = TRUE, 
                               as.is=TRUE) #as.is to keep the it as char
       
-     
-      # writing to a file to be read by convert file
-      write.table(gene.data, file = "tmp.csv", append = FALSE, sep = ",", row.names = FALSE, quote = FALSE, col.names = FALSE)
+      cai.output <- compute.cai(gene.data, genomeID, w, "tmpCDS.csv", "tmpCDS.fasta")
       
-      # converting the sequences to fasta format with python file
-      convertcmd <- "python Write_genecsvtofasta.py"
-      system(convertcmd)
-      
-      # opening the written file and calculating the CAI
-      fasta.genes <- read.fasta(file = "tmp.fasta")
-      
-      # Searching for Mycoplasma and Spiroplasma, using other weight tables
-      match.words <- c("Mycoplasma", "Spiroplasma")
-      # i contains the indices where Myco/Spiro is found
-      i <- grep(paste(match.words, collapse="|"), genome.and.organisms[,2])
-      genomesMycoSpiro <- genome.and.organisms[i,1]
-      # when the genome number of myc/spir is not found it will run the default
-      # else it will run with the codon table of myc+spiroplasma
-      if(!(genomeID %in% genomesMycoSpiro)) {
-      cai.output <- sapply(fasta.genes, cai, w = w, numcode = 1)
-      } else { 
-      cai.output <- sapply(fasta.genes, cai, w = w, numcode = 4)
-      }
-      # write the data to a file
-      cai.df <- data.frame(cai.output)
-      cai.df$gene_id <- names(cai.output)
-      cai.df <- cai.df[,c(2,1)]
-      rownames(cai.df) <- NULL
-      write.table(cai.df, file = fileout, append = F, sep = ",", quote = F, col.names = T, row.names = F)
+      # # writing to a file to be read by convert file
+      # write.table(gene.data, file = "tmp.csv", append = FALSE, sep = ",", row.names = FALSE, quote = FALSE, col.names = FALSE)
+      # 
+      # # converting the sequences to fasta format with python file
+      # convertcmd <- "python Write_genecsvtofasta.py"
+      # system(convertcmd)
+      # 
+      # # opening the written file and calculating the CAI
+      # fasta.genes <- read.fasta(file = "tmp.fasta")
+      # 
+      # # Searching for Mycoplasma and Spiroplasma, using other weight tables
+      # match.words <- c("Mycoplasma", "Spiroplasma")
+      # # i contains the indices where Myco/Spiro is found
+      # i <- grep(paste(match.words, collapse="|"), genome.and.organisms[,2])
+      # genomesMycoSpiro <- genome.and.organisms[i,1]
+      # # when the genome number of myc/spir is not found it will run the default
+      # # else it will run with the codon table of myc+spiroplasma
+      # if(!(genomeID %in% genomesMycoSpiro)) {
+      # cai.output <- sapply(fasta.genes, cai, w = w, numcode = 1)
+      # } else { 
+      # cai.output <- sapply(fasta.genes, cai, w = w, numcode = 4)
+      # }
+      # # write the data to a file
+      # cai.df <- data.frame(cai.output)
+      # cai.df$gene_id <- names(cai.output)
+      # cai.df <- cai.df[,c(2,1)]
+      # rownames(cai.df) <- NULL
+      write.table(cai.output, file = fileout, append = F, sep = ",", quote = F, col.names = T, row.names = F)
 
       }
   }
 }
 
-#############_________________Function for computing cai from SPARQL data_________________#############
-
-compute.cai <- function(seqs.df, genome_ID){
-
-write.table(seqs.df, file = "tmpcai2.csv", append = FALSE, sep = ",", row.names = FALSE, quote = FALSE, col.names = FALSE)
-
-# converting the sequences to fasta format with python file
-convertcmd <- "python Write_genecsvtofasta.py"
-system(convertcmd)
-
-# opening the written file and calculating the CAI
-fasta.genes <- read.fasta(file = "tmp.fasta")
-
-
-#load all the genomeIDs and organisms for checking for Myco/Spiroplasma
-genome.and.organisms <- read.csv(file = "genomes_ENA.csv", 
-                                 header = FALSE, as.is=TRUE) 
-
-# Searching for Mycoplasma and Spiroplasma, using other weight tables
-match.words <- c("Mycoplasma", "Spiroplasma")
-# i contains the indices where Myco/Spiro is found
-i <- grep(paste(match.words, collapse="|"), genome.and.organisms[,2])
-genomesMycoSpiro <- genome.and.organisms[i,1]
-# when the genome number of myc/spir is not found it will run the default
-# else it will run with the codon table of myc+spiroplasma
-if(!(genomeID %in% genomesMycoSpiro)) {
-  cai.output <- sapply(fasta.genes, cai, w = w, numcode = 1)
-} else { 
-  cai.output <- sapply(fasta.genes, cai, w = w, numcode = 4)
-}
-# write the data to a file
-cai.df <- data.frame(cai.output)
-cai.df$gene_id <- names(cai.output)
-cai.df <- cai.df[,c(2,1)]
-rownames(cai.df) <- NULL
-return(cai.df)
-}
-
-
-#compute.cai(gene.data, "GCA_000003645")
+# #############_________________Function for computing cai from SPARQL data_________________#############
+# 
+# compute.cai <- function(seqs.df, genome_ID){
+# 
+# write.table(seqs.df, file = "tmpcai2.csv", append = FALSE, sep = ",", row.names = FALSE, quote = FALSE, col.names = FALSE)
+# 
+# # converting the sequences to fasta format with python file
+# convertcmd <- "python Write_genecsvtofasta.py"
+# system(convertcmd)
+# 
+# # opening the written file and calculating the CAI
+# fasta.genes <- read.fasta(file = "tmp.fasta")
+# 
+# 
+# #load all the genomeIDs and organisms for checking for Myco/Spiroplasma
+# genome.and.organisms <- read.csv(file = "genomes_ENA.csv", 
+#                                  header = FALSE, as.is=TRUE) 
+# 
+# # Searching for Mycoplasma and Spiroplasma, using other weight tables
+# match.words <- c("Mycoplasma", "Spiroplasma")
+# # i contains the indices where Myco/Spiro is found
+# i <- grep(paste(match.words, collapse="|"), genome.and.organisms[,2])
+# genomesMycoSpiro <- genome.and.organisms[i,1]
+# # when the genome number of myc/spir is not found it will run the default
+# # else it will run with the codon table of myc+spiroplasma
+# if(!(genomeID %in% genomesMycoSpiro)) {
+#   cai.output <- sapply(fasta.genes, cai, w = w, numcode = 1)
+# } else { 
+#   cai.output <- sapply(fasta.genes, cai, w = w, numcode = 4)
+# }
+# # write the data to a file
+# cai.df <- data.frame(cai.output)
+# cai.df$gene_id <- names(cai.output)
+# cai.df <- cai.df[,c(2,1)]
+# rownames(cai.df) <- NULL
+# return(cai.df)
+# }
+# 
+# 
+# #compute.cai(gene.data, "GCA_000003645")
