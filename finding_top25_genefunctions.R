b02e63ed2de9230e74ed0202f02cfa397f5cb7e6
meeldurb <meeldurb@gmail.com>
2017-03-02 11:48:51 +0100

    added if statement for existing of file

--- finding_top25_genefunctions.R
+++ finding_top25_genefunctions.R
@@ -16,7 +16,7 @@ setwd("~/Documents/Master_Thesis_SSB/git_scripts")
 # get filenames
 outfolder <- "top25genes/"  
 if (!file.exists(outfolder))dir.create(outfolder)
-#genomeID <- "GCA_000014005"
+genomeID <- "GCA_000014005"
 
 genome.and.organisms <- read.csv(file = "genomes_ENA.csv", header = FALSE, 
                                  as.is=TRUE) #as.is to keep the it as char
@@ -29,35 +29,36 @@ for (genomeID in genome.and.organisms[,1]) {
     top25.files <- paste("Iterated_weight_tables_ENA/", genomeID, "_restop25.csv", sep = "")
     genenames.files <- paste("CDS_data_withgenenames/", genomeID, "_CDS_names.csv", sep = "")
     if (file.exists(top25.files)){
-      # read the top 25 file
-      top25.data <- read.csv(file = top25.files, sep = ",", header = FALSE, as.is = TRUE)
-      # read the CDS with names file
-      genenames.data <- read.csv(file=genenames.files, sep = ",", header = TRUE)
-      # find the top 25 genes in the names file
-      top25 <- as.vector(top25.data[,1])
-      match <- genenames.data[genenames.data[,1] %in% top25, ]
+      if (file.exists(genenames.files)){
+        # read the top 25 file
+        top25.data <- read.csv(file = top25.files, sep = ",", header = FALSE, as.is = TRUE)
+        # read the CDS with names file
+        genenames.data <- read.csv(file=genenames.files, sep = ",", header = TRUE)
+        # find the top 25 genes in the names file
+        top25 <- as.vector(top25.data[,1])
+        match <- genenames.data[genenames.data[,1] %in% top25, ]
       
-      # link both and write new file
-      write.table(match, file=fileout, append=F, sep = ",",
+        # link both and write new file
+        write.table(match, file=fileout, append=F, sep = ",",
                   row.names = F, quote=F, col.names=T )
-      
+      }
     }
   }
 }
-
-# read the top 25 file
-top25.files <- paste("Iterated_weight_tables_ENA/", genomeID, "_restop25.csv", sep = "")
-top25.data <- read.csv(file = top25.files, sep = ",", header = FALSE, as.is = TRUE)
-
-# read the CDS with names file
-genenames.files <- paste("CDS_data_withgenenames/", genomeID, "_CDS_names.csv", sep = "")
-genenames.data <- read.csv(file=genenames.files, sep = ",", colClasses=c(NA, NA, NA, "NULL"), header = TRUE)
-
-
-# find the top 25 genes in the names file
-top25 <- as.vector(top25.data[,1])
-match <- genenames.data[genenames.data[,1] %in% top25, ]
-
-# link both and write new file
-write.table(match, file=fileout, append=F, sep = ",",
-            row.names = F, quote=F, col.names=T )
+# 
+# # read the top 25 file
+# top25.files <- paste("Iterated_weight_tables_ENA/", genomeID, "_restop25.csv", sep = "")
+# top25.data <- read.csv(file = top25.files, sep = ",", header = FALSE, as.is = TRUE)
+# 
+# # read the CDS with names file
+# genenames.files <- paste("CDS_data_withgenenames/", genomeID, "_CDS_names.csv", sep = "")
+# genenames.data <- read.csv(file=genenames.files, sep = ",", colClasses=c(NA, NA, NA, "NULL"), header = TRUE)
+# 
+# 
+# # find the top 25 genes in the names file
+# top25 <- as.vector(top25.data[,1])
+# match <- genenames.data[genenames.data[,1] %in% top25, ]
+# 
+# # link both and write new file
+# write.table(match, file=fileout, append=F, sep = ",",
+#             row.names = F, quote=F, col.names=T )
