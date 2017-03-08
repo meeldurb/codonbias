86e841e18585f67fa6777c186ccd96c39a06ad0e
meeldurb <meeldurb@gmail.com>
2017-03-08 13:10:04 +0100

    colour for biased and unbiased genomes

--- GC_content_vs_CAI.R
+++ GC_content_vs_CAI.R
@@ -26,6 +26,15 @@ genomeID <- "GCA_000003645"
 genome.and.organisms <- read.csv(file = "genomes_ENA.csv", header = FALSE, 
                                  as.is=TRUE) #as.is to keep the it as char
 
+# open the files with biased and unbiased genomes for colouring the plot
+biased.genomes <- read.csv(file = "biased.genomes.csv", header = FALSE, 
+                           as.is=TRUE)
+biased.genomes <- as.vector(as.matrix(biased.genomes))
+
+unbiased.genomes <- read.csv(file = "unbiased.genomes.csv", header = FALSE, 
+                             as.is=TRUE)
+unbiased.genomes <- as.vector(as.matrix(unbiased.genomes))
+
 genomecount = 0
 for (genomeID in genome.and.organisms[,1]){
   cat (genomeID, "\n")
@@ -45,17 +54,28 @@ for (genomeID in genome.and.organisms[,1]){
     xlim = c(10, 90)
     ylim = c(0.1, 1)
     # then plot the GC content against the mean CAI
-    if (genomecount == 0){
+      if(genomeID %in% biased.genomes){
+        print ("biased genome")
+        col = "blue"
+        } else if (genomeID %in% unbiased.genomes){
+          print ("unbiased genome")
+        col = "red"
+        } else {
+        col = "grey"
+        }
+    if (genomecount == 0){    
       plot(GCcont, mean.cai, type = "p", xlim = xlim, ylim = ylim,
-           pch = 18, col = "blue",
+           pch = 18, col = col,
            main = "Average CAI vs. GC content",
            xlab = "GC content (%)", 
            ylab = "Mean CAI")
       grid(NULL, NULL, lty = 6, col = "cornsilk2")
       genomecount = genomecount + 1
-    } else {
-      points(GCcont, mean.cai, pch = 18, col = "blue")
-    } 
+      } else {
+      points(GCcont, mean.cai, pch = 18, col = col)
+      }
+    legend("topright" ,c("biased", "unbiased", "unknown"), cex=1.5, pch=18,
+           col=c("blue", "red", "grey") , bty="n")
   }
 }
 
