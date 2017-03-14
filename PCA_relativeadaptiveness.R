6685e3a37ee3ab05097d165ca0ef56990beaa90f
meeldurb <meeldurb@gmail.com>
2017-03-14 11:49:52 +0100

    draws PCA plot now

--- PCA_relativeadaptiveness.R
+++ PCA_relativeadaptiveness.R
@@ -16,12 +16,12 @@ setwd("~/Documents/Master_Thesis_SSB/git_scripts")
 # open files and making suitable for analysis
 genomeID <- "GCA_000003925"
 # reading a .csv file containing the genome names in the first column
-genome.and.organisms <- read.csv(file = "genomes_ENA.csv", header = FALSE, 
+genome.and.organisms <- read.csv(file = "genomes_ENA_nodupl.csv", header = FALSE, 
                                 as.is=TRUE) #as.is to keep the it as char
 
 
 
-# create dataframe of all genomes with the codons and their relative adaptiveness
+####______________ create dataframe of all genomes with the codons and their relative adaptiveness ______________####
 genomecount = 0
 n = 0
 c = 0
@@ -49,9 +49,80 @@ for (genomeID in genome.and.organisms[,1]){
       c <- c + 1
     }
 }
-write.csv(codgendf, file = "CodonGenomeDataSet.RData")
+write.csv(codgendf, file = "codonGenomeDataSet.csv")
 
 
+####________________________ Do the PCA of all genomes and codon weights ________________________####
+
+setwd("~/Documents/Master_Thesis_SSB/git_scripts")
+
+load("codonGenomeDataSet.RData")
+colnames(codgendf)
+codgen <- t(codgendf)
+rownames(codgen)
+str(codgen)
+
+gold.data= read.table(file = "gold_gca.tsv", sep="\t" , header=TRUE,
+                      row.names=1,as.is=TRUE)
+str(gold.data)
+
+# shorten gold.data for genomeIDs we have in codgendf
+gold.data <- gold.data[gold.data$GCA %in% rownames(codgen), ]
+str(gold.data)
+# shorten also codgen data
+codgen <- codgen[rownames(codgen) %in% gold.data$GCA, ]
+str(codgen)
+
+# do the PCA
+m.codgen <- as.matrix(codgen)
+codgen.pca <- prcomp(m.codgen)
+pca.summary <- summary(codgen.pca)
+
+#easy plot
+PC1.codgen <- as.numeric(codgen.pca$x[,1])
+PC2.codgen <- as.numeric(codgen.pca$x[,2])
+
+plot(PC1.codgen, PC2.codgen, xlab=paste("PC1 (", format(pca.summary$importance[2,1]*100, digits=2),"%)", sep=""), 
+     ylab=paste("PC2 (", format(pca.summary$importance[2,2]*100, digits=2),"%)", sep=""))
+
+
+
+### ggplot
+df <-data.frame(PC1.codgen, PC2.codgen)
+
+genus.count <- rle(sort(gold.data$Genus))
+selected <- genus.count$values[which(genus.count$length>100)]   ##put colors in those for which we have more than 5 (increase for a "real" example
+group <- gold.data$Genus
+group[which(!group%in% selected)] <- "Other"
+
+library(ggplot2)
+df$Group <- group
+df$Group <- factor(df$Group, levels=c(selected, "Other") )
+
+
+myplot <- ggplot(df,aes(PC1.codgen,PC2.codgen, color=Group))+   #these commands creat the plot, but nothing appears
+  geom_point(size=3, shape=15)+
+  theme(axis.text.x = element_text(angle = 0, vjust = 0,  size = 12, hjust = 0.5)) + 
+  theme(axis.text.y = element_text(angle = 0, vjust = 0,  size = 12, hjust = 0.5))+
+  theme_bw()+
+  theme(legend.background = element_rect(fill="white", size=.0, linetype="dotted")) +
+  theme(legend.text = element_text( size = 10))  +
+  xlab( paste("PC1 (", format(pca.summary$importance[2,1]*100, digits=2),"%)", sep="")) +   
+  ylab( paste("PC2 (", format(pca.summary$importance[2,2]*100, digits=2),"%)", sep=""))
+
+
+
+print(myplot)   #have a look at the plot 
+
+ggsave(file="test.pdf", myplot, width=8, height=8)  #save to a file (change extensdion for tiff, png..)
+
+
+
+#another aspect  #DEFAULS
+myplot <- ggplot(df,aes( PC1,PC2, color=Group))  +    #these commands creat the plot, but nothing appears
+  geom_point(size=3, shape=15)
+
+print(myplot) 
 
 ###_______________________________example script from Maria_______________________________###
 setwd("~/Documents/Master_Thesis_SSB/git_scripts/old or unused scripts and data/PCA example")
@@ -61,6 +132,8 @@ str(data)   ###100 samples (genomes) and 70 variables (domains)
 
 load("toyMetaDataSet.RData")  ##this loads the metadata
 str(domdata) #metadata for the genomes
+
+
 
 ##do the PCA
 yy <- as.matrix(data)
