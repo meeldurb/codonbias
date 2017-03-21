4b8b066cf2fd46b669ba6d71cf184a6c02df01f7
meeldurb <meeldurb@gmail.com>
2017-03-21 13:27:17 +0100

    for loop counting codon pairs works for example sequences

--- codonpairs_itweighttable.R
+++ codonpairs_itweighttable.R
@@ -15,10 +15,13 @@ source("http://bioconductor.org/biocLite.R")
 biocLite("Biostrings")
 # to compute CAI
 install.packages("seqinr", repos="http://cran.rstudio.com/")
+# for strings
+install.packages("stringr", repos="http://cran.rstudio.com/")
 
 # loading required libraries
 library("Biostrings")
 library("seqinr")
+library("stringr")
 
 #loading my own functions
 source("/home/melanie/Documents/Master_Thesis_SSB/git_scripts/cweight.R")
@@ -34,32 +37,58 @@ aa <- translate(s2c(c2s(words())))
 names(aa) <- codons
 aa
 # codon table 1 bacterial
-aa1 <- getGeneticCode("SGC1")
+aa1 <- getGeneticCode("SGC0")
 # codon table 4 spiro/myco
-aa4 <- getGeneticCode("SGC4")
+aa4 <- getGeneticCode("SGC3")
 
 
 
+codon.pairs <- NULL
+aa.pairs <- NULL
 
-pairsCodons <- NULL
-pairsAA <- NULL
-
-for ( first in names(transl)){ 
-  for (second in names(transl)) {
+for (first in names(aa1)){ 
+  for (second in names(aa1)) {
     pcodons <- paste(first, second, sep="_")
-    paa <- paste(transl[first], transl[second], sep="_")
-    pairsCodons <- c( pairsCodons,pcodons)
-    pairsAA <- c(pairsAA, paa)
+    paa <- paste(aa1[first], aa1[second], sep="_")
+    codon.pairs <- c(codon.pairs, pcodons)
+    aa.pairs <- c(aa.pairs, paa)
   }
 }
-pairsCodons
-pairsAA
-tranlPairs <- pairsAA
-names(tranlPairs) <- pairsCodons
+
+codonpair.table <- aa.pairs
+names(codonpair.table) <- codon.pairs
+codonpair.table
+
+
+# fake sequence
+seq1<- "AACTGCGATGATACAGGGATACCGATAGACTAGATCCAGGTAACGATAAAGCGAGAGGCC"
+seq2 <- "AACTGCAATGATACAGGGATACGGGCTAACTAGATCCAGGTAACGATAAACATTGGAGGC"
+seqs <- c(seq1, seq2)
+for (DNAseq in seqs){
+  sq <- gsub("(.{3})", "\\1 ", DNAseq)
+  sq <- sub("\\s+$", "", sq)
+  sq <- gsub(" ", "_", sq)
+  print(sq)
+  for (codonpair in names(codonpair.table)){
+    #print (codonpair)
+    meh <- str_count(sq, pattern=codonpair)
+    if (meh > 0){
+      print (codonpair)
+      print (meh)
+    }
+  }
+}
+
+# counting occurence in string
+#str_count(sq, "AAC_TGC")
+
+
+
 
 #fakedata
 data <- sample(1:100, 25)
 names(data) <- pairsCodons
+data
 AAp <- unique(tranlPairs)
 
 for (aap in AAp){
