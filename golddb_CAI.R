2ab2278f076549e6aeba44b44279fbfa59370f4c
meeldurb <meeldurb@gmail.com>
2017-04-06 17:04:45 +0200

    histograms with relative frequencies work

--- golddb_CAI.R
+++ golddb_CAI.R
@@ -9,6 +9,11 @@
 ####  The CAI is calculated for these species and compared with each other
 #################################################################################
 
+install.packages("HistogramTools", repos="http://cran.rstudio.com/")
+library("HistogramTools")
+library("lattice")
+
+
 setwd("~/Documents/Master_Thesis_SSB/git_scripts")
 
 genomeID <- "GCA_000003645"
@@ -37,12 +42,13 @@ genID = character()
 sal = character()
 gencount = 1
 for (genomeID in salinity[,1]){
+  cat (genomeID, "\n")
   cai.files <- paste("new_CAI_CDS/", genomeID, "_CAI_CDS_new.csv", sep = "")
   if (file.exists(cai.files)){
     if (genomeID %in% NeutGen){
       data <- read.csv(file = cai.files, sep = ",", 
                        header = TRUE, as.is = TRUE)
-      mean.cai <- mean(data[gencount,2])
+      mean.cai <- mean(data[,2])
       # setting properties for drawing the graphs
       genID <- c(as.character(genomeID), genID)
       means <- c(mean.cai, means)
@@ -55,89 +61,53 @@ salinity.df <- data.frame(genID, sal, means, stringsAsFactors = TRUE)
 salinity.df <- salinity.df[!salinity.df[,2] == "Undefined", ]
 
 
-# draw the plot (normal R)
-hist(salinity.df[,3], 
-      freq = TRUE,
-      col = salinity.df$sal,
-      xlab="mean CAI", 
-      main="CAI distribution of different salinity requirements", 
-      ylab = "Bacterial frequency")
-#grid(NULL, NULL, lty = 6, col = "cornsilk2")
-legend("topright" ,c("halophile", "halotolerant", "stenohaline", "euryhaline"), cex=1.5, pch=18,
-       col = unique(data.CAIGCpolIII$polIII.col), bty="n")
-
-# setting the properties for drawing the graphs
-xlim <- range(salinity.df[,3], na.rm = TRUE)
-nbins = 50
-par(mfrow = c(1,1))
-
-# drawing histogram with all data inside
-hist(salinity.df[,3], breaks = seq(xlim[1], xlim[2], length = nbins),
-     xlim = xlim)
-
 # retrieving CAI values of each of the conditions
-#euryhal.cai <- salinity.df[which(salinity.df$sal == "Euryhaline"), 3]
+euryhal.cai <- salinity.df[which(salinity.df$sal == "Euryhaline"), 3]
 halophile.cai <- salinity.df[which(salinity.df$sal == "Halophile"), 3]
 halotol.cai <- salinity.df[which(salinity.df$sal == "Halotolerant"), 3]
 stenohal.cai <- salinity.df[which(salinity.df$sal == "Stenohaline"), 3]
 
-par(mfrow = c(2,2))
-# euryhal.hist <- hist(euryhal.cai,
-#                      breaks = seq(xlim[1], xlim[2], length = nbins), xlim = xlim)
+# draw the histogram plot
+xlim <- range(salinity.df[,3], na.rm = TRUE)
+breakpoints <- seq(xlim[1], xlim[2], length.out = 20)
+
+# get the histograms and calculate percentages
+par(mfrow = c(1,1))
+euryhal.hist <- hist(euryhal.cai,
+                     breaks = breakpoints, plot = F)
+euryhal.hist$counts = euryhal.hist$counts/sum(euryhal.hist$counts)
 
 halophile.hist <- hist(halophile.cai,
-                      breaks = seq(xlim[1], xlim[2], length = nbins), xlim = xlim)
-halophile.hist$density = halophile.hist$counts/sum(halophile.hist$counts)*100
+                       breaks = breakpoints, plot = F)
+halophile.hist$counts = halophile.hist$counts/sum(halophile.hist$counts)
 
 halotol.hist <- hist(halotol.cai,
-                       breaks = seq(xlim[1], xlim[2], length = nbins), xlim = xlim)
-halotol.hist$density = halotol.hist$counts/sum(halotol.hist$counts)*100
-
+                     breaks = breakpoints, plot = F)
+halotol.hist$counts = halotol.hist$counts/sum(halotol.hist$counts)
 
 stenohal.hist <- hist(stenohal.cai,
-                     breaks = seq(xlim[1], xlim[2], length = nbins), xlim = xlim)
-stenohal.hist$density = stenohal.hist$counts/sum(stenohal.hist$counts)*100
+                      breaks = breakpoints, plot = F)
+stenohal.hist$counts = stenohal.hist$counts/sum(stenohal.hist$counts)
 
-par(mfrow = c(2,2))
-plot(halophile.hist, freq = FALSE)
-plot(halotol.hist, freq = FALSE)
-plot(stenohal.hist, freq = FALSE)
+plot(stenohal.hist, col = rgb(0,1,1,1/4), 
+     main = "CAI distribution of salinity requirements",
+     xlab = "mean CAI",
+     ylab = "Bacterial frequency")
 
-
-# Overlap the histograms
-
-# setting y-axis limit to draw the combined histogram
-#ylim <- c(0,max(halophile.hist$density, halotol.hist$density, stenohal.hist$density))
-par(mfrow = c(1,1))
-
-plot(halophile.hist, col = rgb(1,1,1/4,1/2), xlim = xlim,  freq=TRUE,
-      xlab="mean CAI", cex = 1.5, main="CAI distribution of different salinity requirements",
-      ylab = "Bacterial frequency", cex.axis = 1.5, cex.lab=1.5)
-
-plot(euryhal.hist, col = rgb(0,0,1,1/4), xlim =  xlim, add = TRUE,  freq = TRUE)
-plot(halotol.hist, col = rgb(1,0,0,1/4), xlim =  xlim, add = TRUE,  freq = TRUE)
-plot(stenohal.hist, col = rgb(0,1,0,1/4), xlim =  xlim, add = TRUE,  freq = TRUE)
-
+plot(halophile.hist, col = rgb(1,0,0,1/4), add = TRUE)
+plot(euryhal.hist, col = rgb(1,1,1/4,1/2), add = TRUE)
+plot(halotol.hist, col = rgb(0,0,1,1/4), add = TRUE)
 legend("topright", c("Halophile", "Euryhaline", "Halotolerant", "Stenohaline"), pch = 15,
-       col = c(rgb(1,1,1/4,1/2), rgb(0,0,1,1/4), rgb(1,0,0,1/4), rgb(0,1,0,1/4)) , bty = "n", cex=1.5)
+        col = c(rgb(1,0,0,1/4), rgb(1,1,1/4,1/2), rgb(0,0,1,1/4), rgb(0,1,1,1/4)) , bty = "n", cex=1.5)
+
+
 
 # removed eury- and stenohaline
-ylim <- c(0,max(halophile.hist$density, halotol.hist$density))
-
-
 par(mfrow = c(1,1))
-
-plot(halophile.hist, col = rgb(0,0,1,1/4), xlim = xlim,  freq=FALSE,
-     xlab="mean CAI", cex = 1.5, main="CAI distribution of different salinity requirements",
-     ylab = "Bacterial frequency", cex.axis = 1.5, cex.lab=1.5, ylim = ylim)
-
-# plot(euryhal.hist, col = 'yellow', xlim =  xlim, add = TRUE,  freq = FALSE)
-plot(halotol.hist, col = rgb(1,0,0,1/4), xlim =  xlim, add = TRUE,  freq = FALSE)
-#plot(stenohal.hist, col = 'green', xlim =  xlim, add = TRUE,  freq = FALSE)
-
-legend("topright", c("Halophile", "Halotolerant"), pch = 15,
-       col = c(rgb(0,0,1,1/4),rgb(1,0,0,1/4)), bty = "n", cex=1.5)
-
+plot(halotol.hist, col = rgb(1,0,0,1/4))
+plot(halophile.hist, col = rgb(0,0,1,1/4), add = TRUE)
+legend("topleft", c("Halophile", "Halotolerant"), pch = 15,
+       col = c(rgb(1,0,0,1/4), rgb(0,0,1,1/4)) , bty = "n", cex=1.5)
 
 ##############_______________________________Oxygen requirement_______________________________#####################
 # taking the genomeIDs that have oxygen information
@@ -161,7 +131,7 @@ for (genomeID in oxygen[,1]){
     if (genomeID %in% NeutGen){
       data <- read.csv(file = cai.files, sep = ",", 
                        header = TRUE, as.is = TRUE)
-      mean.cai <- mean(data[gencount,2])
+      mean.cai <- mean(data[,2])
       # setting properties for drawing the graphs
       genID <- c(as.character(genomeID), genID)
       means <- c(mean.cai, means)
@@ -170,19 +140,11 @@ for (genomeID in oxygen[,1]){
     }
   }
 }
+# write all the data to a dataframe and remove NA rows
 oxygen.df <- data.frame(genID, oxy, means, stringsAsFactors = FALSE)
+oxygen.df <- na.omit(oxygen.df)
 length(oxygen.df[,1])
 
-
-
-# setting the properties for drawing the graphs
-xlim <- range(oxygen.df[,3], na.rm = TRUE)
-nbins = 20
-par(mfrow = c(1,1))
-
-# drawing histogram with all data inside 
-hist(oxygen.df[,3], breaks = seq(xlim[1], xlim[2], length = nbins), 
-     xlim = xlim)
 
 # retrieving CAI values of each of the conditions
 aerobe.cai <- oxygen.df[which(oxygen.df$oxy == "Aerobe"), 3]
@@ -191,66 +153,74 @@ anaerobe.cai <- oxygen.df[which(oxygen.df$oxy == "Anaerobe"), 3]
 length(anaerobe.cai)
 facul.cai <- oxygen.df[which(oxygen.df$oxy == "Facultative"), 3]
 micr.cai <- oxygen.df[which(oxygen.df$oxy == "Microaerophilic"), 3]
+length(micr.cai)
 oblaerobe.cai <- oxygen.df[which(oxygen.df$oxy == "Obligate aerobe"), 3]
 oblanaerobe.cai <- oxygen.df[which(oxygen.df$oxy == "Obligate anaerobe"), 3]
 
-par(mfrow = c(2,3))
-aerobe.hist <- hist(aerobe.cai, 
-                     breaks = seq(xlim[1], xlim[2], length = nbins), xlim = xlim)
+# draw the histogram plot
+xlim <- range(oxygen.df[,3], na.rm = TRUE)
+breakpoints <- seq(xlim[1], xlim[2], length.out = 20)
 
-anearobe.hist <- hist(anaerobe.cai, 
-                       breaks = seq(xlim[1], xlim[2], length = nbins), xlim = xlim)
-
-facul.hist <- hist(facul.cai, 
-                     breaks = seq(xlim[1], xlim[2], length = nbins), xlim = xlim)
-
-micr.hist <- hist(micr.cai, 
-                      breaks = seq(xlim[1], xlim[2], length = nbins), xlim = xlim)
-
-oblaerobe.hist <- hist(oblaerobe.cai, 
-                  breaks = seq(xlim[1], xlim[2], length = nbins), xlim = xlim)
-
-oblanearobe.hist <- hist(oblanaerobe.cai, 
-                  breaks = seq(xlim[1], xlim[2], length = nbins), xlim = xlim)
-
-
-# Overlap the histograms
-
-# setting y-axis limit to draw the combined histogram
-ylim <- c(0.4, max(aerobe.hist$density, anearobe.hist$density, facul.hist$density, 
-                micr.hist$density, oblaerobe.hist$density, oblanearobe.hist$density))
+# get the histograms and calculate percentages
 par(mfrow = c(1,1))
+aerobe.hist <- hist(aerobe.cai,
+                     breaks = breakpoints, plot = F)
+aerobe.hist$counts = aerobe.hist$counts/sum(aerobe.hist$counts)
 
-plot(aerobe.hist, col = rgb(1,1,1/4,1/2), xlim = xlim,  freq=FALSE,
-     xlab="mean CAI", cex = 1.5, main="CAI distribution of different oxygen requirements", 
-     ylab = "Bacterial frequency", cex.axis = 1.5, cex.lab=1.5, ylim = ylim) 
+anaerobe.hist <- hist(anaerobe.cai,
+                       breaks = breakpoints, plot = F)
+anaerobe.hist$counts = anaerobe.hist$counts/sum(anaerobe.hist$counts)
 
-plot(anearobe.hist, col = rgb(0,0,1,1/4), xlim =  xlim, add = TRUE,  freq = FALSE)
-plot(facul.hist, col = rgb(1,0,0,1/4), xlim =  xlim, add = TRUE,  freq = FALSE)
-plot(micr.hist, col = rgb(0,1,0,1/4), xlim =  xlim, add = TRUE,  freq = FALSE)
-plot(oblaerobe.hist, col = rgb(0,1,1,1/2), xlim =  xlim, add = TRUE,  freq = FALSE)
-plot(oblanearobe.hist, col = rgb(1,1/4,1,1/2), xlim =  xlim, add = TRUE,  freq = FALSE)
+facul.hist <- hist(facul.cai,
+                     breaks = breakpoints, plot = F)
+facul.hist$counts = facul.hist$counts/sum(facul.hist$counts)
+
+micro.hist <- hist(micr.cai,
+                      breaks = breakpoints, plot = F)
+micro.hist$counts = micro.hist$counts/sum(micro.hist$counts)
+
+oblaerobe.hist <- hist(oblaerobe.cai,
+                   breaks = breakpoints, plot = F)
+oblaerobe.hist$counts = oblaerobe.hist$counts/sum(oblaerobe.hist$counts)
+
+oblanaerobe.hist <- hist(oblanaerobe.cai,
+                   breaks = breakpoints, plot = F)
+oblanaerobe.hist$counts = oblanaerobe.hist$counts/sum(oblanaerobe.hist$counts)
+
+plot(facul.hist, col = rgb(1,0,0,1/4),  add = TRUE,
+     main = "CAI distribution of oxygen requirements",
+     xlab = "mean CAI",
+     ylab = "Bacterial frequency") 
+plot(anaerobe.hist, col = rgb(0,0,1,1/4), add = TRUE)
+plot(micro.hist, col = rgb(0,1,0,1/4), add = TRUE)
+plot(aerobe.hist, col = rgb(1,1,1/4,1/2), add = TRUE)
+plot(oblaerobe.hist, col = rgb(0,1,1,1/2), add = TRUE)
+plot(oblanaerobe.hist, col = rgb(1,1/4,1,1/2), add = TRUE)
 
 
-legend("topright", c("Aerobe", "Anearobe", "Facultative", "Microareophilic", 
+legend("topleft", c("Aerobe", "Anearobe", "Facultative", "Microareophilic", 
                      "Obligate aerobe", "Obligate anaerobe"), pch = 15,
-                      col = c(rgb(1,1,1/4,1/2), rgb(0,0,1,1/4), rgb(1,0,0,1/4), 
-                              rgb(0,1,0,1/4), rgb(0,1,1,1/2), rgb(1,1/4,1,1/2)), bty = "n", cex=1.5)
+       col = c(rgb(1,1,1/4,1/2), rgb(0,0,1,1/4), rgb(1,0,0,1/4), 
+               rgb(0,1,0,1/4), rgb(0,1,1,1/2), rgb(1,1/4,1,1/2)), bty = "n", cex=1.5)
+
+
+plot(stenohal.hist, col = rgb(0,1,1,1/4), 
+     main = "CAI distribution of salinity requirements",
+     xlab = "mean CAI",
+     ylab = "Bacterial frequency")
+
+plot(halophile.hist, col = rgb(1,0,0,1/4), add = TRUE)
+plot(euryhal.hist, col = rgb(1,1,1/4,1/2), add = TRUE)
+plot(halotol.hist, col = rgb(0,0,1,1/4), add = TRUE)
+legend("topright", c("Halophile", "Euryhaline", "Halotolerant", "Stenohaline"), pch = 15,
+       col = c(rgb(1,0,0,1/4), rgb(1,1,1/4,1/2), rgb(0,0,1,1/4), rgb(0,1,1,1/4)) , bty = "n", cex=1.5)
+
+
 
 # removed eury- and stenohaline
-ylim <- c(0,max(halophile.hist$density, halotol.hist$density))
-
-
 par(mfrow = c(1,1))
-
-plot(halophile.hist, col = rgb(0,0,1,1/4), xlim = xlim,  freq=FALSE,
-     xlab="mean CAI", cex = 1.5, main="CAI distribution of different salinity requirements", 
-     ylab = "Bacterial frequency", cex.axis = 1.5, cex.lab=1.5, ylim = ylim) 
-
-# plot(euryhal.hist, col = 'yellow', xlim =  xlim, add = TRUE,  freq = FALSE)
-plot(halotol.hist, col = rgb(1,0,0,1/4), xlim =  xlim, add = TRUE,  freq = FALSE)
-#plot(stenohal.hist, col = 'green', xlim =  xlim, add = TRUE,  freq = FALSE)
-
-legend("topright", c("Halophile", "Halotolerant"), pch = 15,
-       col = c(rgb(0,0,1,1/4),rgb(1,0,0,1/4)), bty = "n", cex=1.5)
+plot(halotol.hist, col = rgb(1,0,0,1/4))
+plot(halophile.hist, col = rgb(0,0,1,1/4), add = TRUE)
+legend("topleft", c("Halophile", "Halotolerant"), pch = 15,
+       col = c(rgb(1,0,0,1/4), rgb(0,0,1,1/4)) , bty = "n", cex=1.5)
 
