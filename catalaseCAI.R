165b16c6a9c421508868f374bdb30d31b19a2182
meeldurb <meeldurb@gmail.com>
2017-04-20 14:02:45 +0200

    splitted on writing catalase genes and calculating CAI and drawing the plot

--- catalaseCAI.R
+++ catalaseCAI.R
@@ -71,30 +71,16 @@ FILTER(!regex(?domain_description, " "))
   
 ENDPOINT = "http://ssb2.wurnet.nl:7201/repositories/ENA"
 
-genomecount = 0
 for (genomeID in genome.and.organisms[,1]){
   cat (genomeID, "\n")
   fileout <- paste(outfolder, genomeID, "_cat.csv", sep="")
   #check if file already exists
   if (!file.exists(fileout)) {
-
+    # check whether CAI and w files exists, else the catalase CAI cannot be calculated anyway
     cai.files <- paste("new_CAI_CDS/", genomeID, "_CAI_CDS_new.csv", sep = "")
       if (file.exists(cai.files)){
-        cai.data <- read.csv(file = cai.files, header = TRUE, 
-                           as.is=TRUE) #as.is to keep the it as char
-        mean.cai <- mean(cai.data[,2])
-      
-        w.files <- paste("Iterated_weight_tables_ENA/", genomeID, "_it_weight.csv", sep = "")
+       w.files <- paste("Iterated_weight_tables_ENA/", genomeID, "_it_weight.csv", sep = "")
         if (file.exists(w.files)){
-          w.data <- read.csv(file = w.files, header = FALSE, as.is = TRUE)
-          # ordering on rownames, for it to be correctly used by cai function
-          ordered.w <- w.data[with(w.data, order(V1)), ]
-          # only leaving numbers
-          w <- ordered.w[,2]
-          
-
-          
-          
           #####~~~~~~~~~~~~~~~~~~~~~~~~~ Retrieving catalase protein genes ~~~~~~~~~~~~~~~~~~~~~~~~~#####
           
           sub.query.catalase.seqs <- sub("xxx", genomeID, query.catalase.seqs)
@@ -112,58 +98,88 @@ for (genomeID in genome.and.organisms[,1]){
             next 
           }
           write.table(catalase.seqs.data, file = fileout, append = F, sep = ",", row.names = F, quote = F, col.names = T)
-          catalase.seqs <- read.csv(file = fileout, sep = ",", header = TRUE, as.is = TRUE)
-          # computing the postions of the domains with their corresponding postion in the CDS
-          for (row in catalase.seqs){
-            CDS_begin <- 3*(catalase.seqs[5]-1)+1
-            CDS_end <- 3*(catalase.seqs[6])
-          }
-          
-          # binding these columns to already existing dataframe
-          # and converting to integers for later calculation
-          catalase.seqs$CDS_domain_beginpos <- as.integer(CDS_begin[,1])
-          catalase.seqs$CDS_domain_end <- as.integer(CDS_end[,1])
-          
-          # Substracting the CDS part that is associated to the protein domain
-          # from columns 5 and 6 the positions of the begin and end of the 
-          # CDS associated to protein domain are derived
-          # when CDS are equal, take the integer from that column
-          for (CDS in catalase.seqs[4]){
-            dom_seq <- substr(CDS, catalase.seqs[catalase.seqs[,4] == CDS, 7], 
-                              catalase.seqs[catalase.seqs[,4] == CDS, 8])
-          }
-          catalase.seqs$CDS_domain <- dom_seq 
-          
-          
-          # shrinking the data to reduce computational time
-          colnames(catalase.seqs) <- c("domain_ID", "domain_description", "genename", "CDS", 
-                                       "d_begin","d_end", "CDS_beginpos", "CDS_end", "CDS_domain")
-          keep <- c("domain_ID", "CDS_domain")
-          catalase.seqs.CDS <- catalase.seqs[keep]
-          
-          cai.output <- compute.cai(catalase.seqs.CDS, genomeID, w, "tmpcatadomain.csv", "tmpcatadomain.fasta")
-          catalase.cai <- mean(cai.output[,2])
-          # draw the plot of mean cai and mean catalase cai
-          xlim = c(0.1, 0.9)
-          ylim = c(0.1, 0.9)
-          col = "blue"
-          if (genomecount == 0){  
-            plot(catalase.cai, mean.cai, col=col, 
-                 xlim = xlim, ylim = ylim,
-                 pch = 18, main = "Average CAI vs. GC content",
-                 xlab = "catalase CAI", 
-                 ylab = "Mean CAI")
-            grid(NULL, NULL, lty = 6, col = "cornsilk2")
-            #legend("bottomright" ,c("dnaE1", "dnaE2/dnaE1", "polC/dnaE3", "Other"), cex=1.5, pch=18,
-            #    col=c("red", "blue", "green", "grey") , bty="n")
-            genomecount = genomecount + 1
-          } else {
-            points(catalase.cai, mean.cai, col=col, pch = 18)
-          }
-          abline(a=0, b=1)
         }
       }
   }
+}
+  
+  
+####__________________after catalase seq files were written, calculate CAI and draw plot______________________#####
+
+genomecount = 0
+for (genomeID in genome.and.organisms[,1]){
+  cat (genomeID, "\n")
+  cai.files <- paste("new_CAI_CDS/", genomeID, "_CAI_CDS_new.csv", sep = "")
+  if (file.exists(cai.files)){
+    cai.data <- read.csv(file = cai.files, header = TRUE, 
+                         as.is=TRUE) #as.is to keep the it as char
+    mean.cai <- mean(cai.data[,2])
+    
+    w.files <- paste("Iterated_weight_tables_ENA/", genomeID, "_it_weight.csv", sep = "")
+    if (file.exists(w.files)){
+      w.data <- read.csv(file = w.files, header = FALSE, as.is = TRUE)
+      # ordering on rownames, for it to be correctly used by cai function
+      ordered.w <- w.data[with(w.data, order(V1)), ]
+      # only leaving numbers
+      w <- ordered.w[,2]
+    
+      
+      filein <- paste(outfolder, genomeID, "_cat.csv", sep="")
+      if (file.exists(filein)){
+        
+        catalase.seqs <- read.csv(file = filein, sep = ",", header = TRUE, as.is = TRUE)
+        # computing the postions of the domains with their corresponding postion in the CDS
+        for (row in catalase.seqs){
+          CDS_begin <- 3*(catalase.seqs[5]-1)+1
+          CDS_end <- 3*(catalase.seqs[6])
+        }
+        
+        # binding these columns to already existing dataframe
+        # and converting to integers for later calculation
+        catalase.seqs$CDS_domain_beginpos <- as.integer(CDS_begin[,1])
+        catalase.seqs$CDS_domain_end <- as.integer(CDS_end[,1])
+        
+        # Substracting the CDS part that is associated to the protein domain
+        # from columns 5 and 6 the positions of the begin and end of the 
+        # CDS associated to protein domain are derived
+        # when CDS are equal, take the integer from that column
+        for (CDS in catalase.seqs[4]){
+          dom_seq <- substr(CDS, catalase.seqs[catalase.seqs[,4] == CDS, 7], 
+                            catalase.seqs[catalase.seqs[,4] == CDS, 8])
+        }
+        catalase.seqs$CDS_domain <- dom_seq 
+        
+        
+        # shrinking the data to reduce computational time
+        colnames(catalase.seqs) <- c("domain_ID", "domain_description", "genename", "CDS", 
+                                     "d_begin","d_end", "CDS_beginpos", "CDS_end", "CDS_domain")
+        keep <- c("domain_ID", "CDS_domain")
+        catalase.seqs.CDS <- catalase.seqs[keep]
+        
+        cai.output <- compute.cai(catalase.seqs.CDS, genomeID, w, "tmpcatadomain.csv", "tmpcatadomain.fasta")
+        catalase.cai <- mean(cai.output[,2])
+        # draw the plot of mean cai and mean catalase cai
+        xlim = c(0.1, 0.9)
+        ylim = c(0.1, 0.9)
+        col = "blue"
+        if (genomecount == 0){  
+          plot(catalase.cai, mean.cai, col=col, 
+               xlim = xlim, ylim = ylim,
+               pch = 18, main = "CAI complete genome vs. CAI catalase genes",
+               xlab = "catalase CAI", 
+               ylab = "Mean CAI")
+          grid(NULL, NULL, lty = 6, col = "cornsilk2")
+          #legend("bottomright" ,c("dnaE1", "dnaE2/dnaE1", "polC/dnaE3", "Other"), cex=1.5, pch=18,
+          #    col=c("red", "blue", "green", "grey") , bty="n")
+          genomecount = genomecount + 1
+        } else {
+          points(catalase.cai, mean.cai, col=col, pch = 18)
+        }
+        abline(a=0, b=1)
+      }
+    }
   }
-
-
+}
+  
+  
+  
