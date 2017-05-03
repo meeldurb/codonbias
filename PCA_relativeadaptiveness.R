fde5b1b0a0e9eeafcc8194e7f04934dee8538c44
Melanie vd Bosch <meeldurb@gmail.com>
2017-05-03 15:20:49 +0200

    changed ordering of the codons and removed myco/spiro

--- PCA_relativeadaptiveness.R
+++ PCA_relativeadaptiveness.R
@@ -29,14 +29,23 @@ for (genomeID in genome.and.organisms[,1]){
   cat (genomeID, "\n")  
   w.files <- paste("Iterated_weight_tables_ENA/", genomeID, "_it_weight.csv", sep = "") 
   if (file.exists(w.files)){
+    # Searching for Mycoplasma and Spiroplasma, which use other genetic codon table
+    match.words <- c("Mycoplasma", "Spiroplasma")
+    # i contains the indices where Myco/Spiro is found
+    i <- grep(paste(match.words, collapse="|"), genome.and.organisms[,2])
+    genomesMycoSpiro <- genome.and.organisms[i,1]
+    # when the genome number of myc/spir is not found it will combine the genomeID in the dataframe
+    # else the dataframe is too short (less stop codons are omitted)
+    # so it will not complain anymore
+    if(!(genomeID %in% genomesMycoSpiro)) {
     n <- n + 1
     w.data <- read.csv(file = w.files, header = FALSE, as.is = TRUE)
     # order on codon because of cai function
-    ordered.w <- w.data[with(w.data, order(w.data[,1])), ]
+    #ordered.w <- w.data[with(w.data, order(w.data[,1])), ]
     # only leaving numbers
-    w <- ordered.w[,2]
+    w <- w.data[,2]
     if (genomecount == 0){
-    codgendf <- data.frame(row.names=ordered.w[,1])
+    codgendf <- data.frame(row.names=w.data[,1])
     codgendf <- cbind(codgendf, w)
     colnames(codgendf)[n] <- genomeID
     genomecount = genomecount + 1
@@ -44,7 +53,10 @@ for (genomeID in genome.and.organisms[,1]){
       codgendf <- cbind(codgendf, w)
       colnames(codgendf)[n] <- genomeID
     } 
-    } else {
+    }else{
+      print(paste("genomefile", "myco/spiro plasma"))
+    }
+    }else {
       print(paste("genomefile", genomeID, "does not exist"))
       c <- c + 1
     }
